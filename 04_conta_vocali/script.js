00eca058e6f3e204a35e25951e40f0393f8cf704
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-09 16:34:31 +0100

    "conta-vocali"

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,36 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+/**
+ * Scrivi una funzione che accetti una stringa
+ * e restituisca il numero di vocali contenute al suo interno
+ */
 
-const word = 'javascript';
+const word = "javascript";
 
+const vocals = ["a", "e", "i", "o", "u"];
+
+let vocalsCount = 0;
+let matchingChar = [];
 
 // Dichiara la funzione qui.
+function vocalsCounter() {
+  const wordSplitted = word.split("");
+  console.log(wordSplitted);
 
+  for (let i = 0; i < wordSplitted.length; i++) {
+    const currentChar = wordSplitted[i];
+    console.log(currentChar);
+    for (let i = 0; i < vocals.length; i++) {
+      const currentVocal = vocals[i];
+      if (currentChar === currentVocal) {
+        console.log("vocal:", currentChar);
+        vocalsCount += 1;
+        matchingChar.push(currentChar);
+      }
+    }
+  }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+vocalsCounter();
+console.log(`${vocalsCount} (${matchingChar})`);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
