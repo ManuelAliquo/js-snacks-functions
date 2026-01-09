8259b45afcb47bede58608eaeb4d157cbf0d5c31
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-09 15:46:04 +0100

    "iniziali"

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,31 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/**
+ * Scrivi una funzione che accetti un array di nomi
+ * e restituisca un nuovo array con le iniziali
+ * di ogni parola dell'array fornito
+ */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+const namesInitial = [];
 
 // Dichiara la funzione qui.
+function initialsFinder() {
+  for (let i = 0; i < names.length; i++) {
+    let currentName = names[i];
+    let splittedName = currentName.split("");
 
+    for (let i = 0; i < splittedName.length; i++) {
+      let currentChar = splittedName[i];
+
+      if (splittedName.indexOf(currentChar) === 0) {
+        namesInitial.push(currentChar);
+      }
+    }
+  }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+initialsFinder(names);
+console.log(namesInitial);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
